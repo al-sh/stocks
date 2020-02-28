d35aced954b5cd846bc1b697e24116898d478157
Aleksandr Shirochkin <shirochkin.as@gmail.com>
2020-02-28 10:06:05 +0500

    Updates

---
+++ precache-manifest.57438c4885c582c103e6d0a434a0339c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f34488241407d20b7bdd0d84df6a48",
+    "url": "/stocks/index.html"
+  },
+  {
+    "revision": "9676c835ceb0d48a412b",
+    "url": "/stocks/static/css/main.b4ac81d4.chunk.css"
+  },
+  {
+    "revision": "13b019cfe0232bae14bd",
+    "url": "/stocks/static/js/2.6cb59669.chunk.js"
+  },
+  {
+    "revision": "cc835925ffb0aaeea483c5e8a2900598",
+    "url": "/stocks/static/js/2.6cb59669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9676c835ceb0d48a412b",
+    "url": "/stocks/static/js/main.fb9026a4.chunk.js"
+  },
+  {
+    "revision": "447aaf141a5d71f24150",
+    "url": "/stocks/static/js/runtime-main.406d27c4.js"
+  }
+]);
