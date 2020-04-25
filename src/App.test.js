b83a49e8c72a3dab2a9aa75926c10dd8e53687ee
Aleksandr Sh <shirochkin.as@gmail.com>
2020-04-25 16:49:40 +0500

    Fix of test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { defaultState } from './store/defaultState'
+import rootReducer from './store/reducers/mainReducer'
+
+import { mSort } from './components/functions';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  mSort(defaultState.items, defaultState.sortParams);
+  let preLoadedState = defaultState;
+
+	const store = createStore(
+		rootReducer, preLoadedState
+	);
+
+	ReactDOM.render(
+		<Provider store={store}>
+			<App />
+		</Provider>,
+		div
+	);
+
   ReactDOM.unmountComponentAtNode(div);
 });
