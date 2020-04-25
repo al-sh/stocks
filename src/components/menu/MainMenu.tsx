2667f915ecfe15f35a38e0a9c51fdaa336a8d74a
Aleksandr Sh <shirochkin.as@gmail.com>
2020-04-25 17:28:55 +0500

    MainMenu -> .ts

--- src/components/menu/MainMenu.tsx
+++ src/components/menu/MainMenu.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {actSwitchToSection} from '../../store/actions' 
+import { IState } from '../../store/interfaces'
 
-class MainMenu extends React.Component {
+interface MainMenuProps {
+  dispatch: any,
+  currentSection: string
+}
+
+const mapStateToProps = (state: IState) => {
+  return {  
+    currentSection: state.currentSection
+  }
+}
+
+class MainMenu extends React.Component<MainMenuProps, IState> {
   testDataWarnText = 'Отсутствует подключение к БД, показаны тестовые данные';
 
-  switchToTransfers = async (e) => {
+  switchToTransfers = async (e: any) => {
     actSwitchToSection('transfers', this.props.dispatch);
   }
 
-  switchToPortfolios = async (e) => {
+  switchToPortfolios = async (e: any) => {
     actSwitchToSection('portfolios', this.props.dispatch);
   }
   
@@ -20,12 +32,6 @@ class MainMenu extends React.Component {
         <span onClick={this.switchToPortfolios} className={this.props.currentSection==='portfolios'? "MainMenu__Item MainMenu__Item-active" : "MainMenu__Item" }>Портфели</span>       
       </div>      
     );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {  
-    currentSection: state.currentSection
   }
 }
 
